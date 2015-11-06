30806580e0824fd5f353dfe12a6707c738794339
lzq420241 <lzq.hdu@gmail.com>
2015-11-06 17:52:04 +0800

    add trend analysis

---
+++ analysis_trend.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import pandas as pd
+import tushare as ts
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+df = pd.read_csv('trend_000883.csv')
+
+
